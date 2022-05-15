2bfc69755664cc5535f4c938fa975cd2d464e17a
IsaacShelton <isaac.a.shelton@gmail.com>
2022-05-14 21:17:27 -0500

    Working on e2e runner so it can handle ANSI escape sequences

--- tests/e2e/e2e-runner.py
+++ tests/e2e/e2e-runner.py
@@ -5,6 +5,6 @@ from framework import test, e2e_framework_run
 
 def run_all_tests():
     executable = sys.argv[1]
-    test([executable], "Hello World\n")
+    test([executable], b'Hello World\n')
 
 e2e_framework_run(run_all_tests)
