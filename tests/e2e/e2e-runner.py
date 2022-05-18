a58b836c19a4703f887c0de271e47244bf850098
IsaacShelton <isaac.a.shelton@gmail.com>
2022-05-18 16:06:50 -0500

    Try first e2e test

--- tests/e2e/e2e-runner.py
+++ tests/e2e/e2e-runner.py
@@ -5,6 +5,6 @@ from framework import test, e2e_framework_run
 
 def run_all_tests():
     executable = sys.argv[1]
-    test([executable], b'Hello World\n')
+    test([executable], b"NOTE: Checking for updates as scheduled in 'adept.config'\n -> Already up to date!\n     /\xe2\xe2\\\n    /    \\    \n   /      \\    \n  /   /\\   \\        The Adept Compiler v2.7 - (c) 2016-2022 Isaac Shelton\n /   /\\__   \\\n/___/    \\___\\\n\nUsage: adept [options] [filename]\n\nOptions:\n    -h, --help        Display this message\n    -e                Execute resulting executable\n    -w                Disable compiler warnings\n    -o FILENAME       Output to FILENAME (relative to working directory)\n    -n FILENAME       Output to FILENAME (relative to file)\n    -c                Emit object file\n    -O0,-O1,-O2,-O3   Set optimization level\n    --windowed        Don't open console with executable (only applies to Windows)\n    -std=2.x          Set standard library version\n    --version         Display compiler version\n    --root            Display root folder\n    --help-advanced   Show lesser used compiler flags\n")
 
 e2e_framework_run(run_all_tests)
